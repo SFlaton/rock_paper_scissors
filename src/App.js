ef8b33aa1d1d7b5c81a235dae3b2fd4baab1ca44
Sflaton <suzanne.flaton@gmail.com>
2016-05-02 14:05:07 +0200

    newplayer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import GameModel from './models/GameModel';
+import NewGameComponent from './components/NewGameComponent';
+import GameListComponent from './components/GameListComponent';
 
 class App extends React.Component {
+    constructor(){
+      super();
+
+      this.state = {
+        games: []
+      };
+    }
+
+    componentDidMount() {
+      this.games = new GameModel();
+      this.games.subscribe(this.updateList.bind(this));
+
+    }
+
+    updateList() {
+      this.setState({
+        games: this.games.resources
+      });
+    }
+
+    createGame(newPlayer) {
+      this.games.addResource({
+        playerOne:newPlayer
+      });
+    }
+
+    containerStyles() {
+      return {
+        width: "500px",
+        height: "500px",
+        margin: "auto"
+      }
+    }
+
+    headerStyle(){
+      return {
+        textAlign: "center"
+      }
+    }
+
     render() {
         return (
-            <h1>Hello World!</h1>
+            <div style={this.containerStyles()}>
+              <h1 style={this.headerStyle()}>Rock Paper Scissors</h1>
+              <NewGameComponent onCreate={this.createGame.bind(this)}/>
+              <GameListComponent games={this.state.games} />
+            </div>
         );
     }
 }
--- src/lib/Utils.js
+++ src/lib/Utils.js
@@ -41,3 +41,5 @@ class Utils {
 		return newObj;
 	}
 }
+
+export default Utils;
